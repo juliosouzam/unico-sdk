5ac2aefacc90a63dd4c673bf71c18e7d272f429c
Júlio César <julio.souzam@gmail.com>
2022-06-04 19:36:10 -0300

    chore: fixes

--- src/unico/index.ts
+++ src/unico/index.ts
@@ -1,7 +1,13 @@
+// Errors
+export * from "./errors";
+
 // HTTP Client and Authentication
 export * from "./secret-key";
 export * from "./auth-client";
 export * from "./unico-client";
+
+// Schema
+export * from "./process/schema";
 
 // Process
 export * from "./process";
